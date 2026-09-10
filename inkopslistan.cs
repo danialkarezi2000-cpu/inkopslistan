8801fd334a8e7208593d35c844e326bccb310614
Danial Karezi <danialkarezi2000@gmail.com>
2026-09-10 18:30:20 +0200

    Lägg till grund för inköpslistan

---
+++ inkopslistan.cs
@@ -0,0 +1,13 @@
+//Lista som sparar namnet på varje vara
+List<string> names = new List<string>();
+//Lisa som sparar priset på varje vara
+List<int> prices = new List<int>();
+
+while (true)
+{
+    Console.WriteLine("InköpsLista");
+    Console.Write("Skriv en vara eller ett nummer:");
+
+    //Sparar användarens inmatning som text
+    string input = Console.ReadLine()!;
+}
