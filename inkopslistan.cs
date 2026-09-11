81b5bb3955215a372c812592f9f66429a829241b
Danial Karezi <danialkarezi2000@gmail.com>
2026-09-11 11:47:13 +0200

    Visa inköpslistan

--- inkopslistan.cs
+++ inkopslistan.cs
@@ -1,11 +1,19 @@
 //Lista som sparar namnet på varje vara
 List<string> names = new List<string>();
+
 //Lisa som sparar priset på varje vara
 List<int> prices = new List<int>();
 
 while (true)
 {
     Console.WriteLine("InköpsLista");
+
+
+    // visar alla varor i listan
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {names[i]} - {prices[i]} kr");
+    }
     Console.Write("Skriv en vara eller ett nummer:");
 
     //Sparar användarens inmatning som text
